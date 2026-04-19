c08649ccae0ac5346101231f768176d99e3a284b
winglol96 <may964538@gmail.com>
2026-04-20 01:28:40 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -250,8 +250,8 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
-        subTitle="Build your own AI assistant."
+        title="DarkLott Works"
+        subTitle="SSM Registered Company."
         logo={<ChatGptIcon />}
         shouldNarrow={shouldNarrow}
       >
